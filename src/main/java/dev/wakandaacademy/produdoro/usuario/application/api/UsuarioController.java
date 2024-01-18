b320b7a821dd16466fcfc1ec22c086637cf5df29
Erik Alves <erikesantos18@icloud.com>
2024-01-18 10:51:03 -0300

    finaliza controller mudaStatusPausaLonga

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
@@ -2,9 +2,12 @@ package dev.wakandaacademy.produdoro.usuario.application.api;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import dev.wakandaacademy.produdoro.handler.APIException;
 import dev.wakandaacademy.produdoro.usuario.application.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -17,6 +20,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
 	private final UsuarioService usuarioAppplicationService;
+	private final TokenService tokenService;
+	
 
 	@Override
 	public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
@@ -37,6 +42,9 @@ public class UsuarioController implements UsuarioAPI {
 	public void mudaStatusPausaLonga(String token, UUID idUsuario) {
 		log.info("[inicia] UsuarioController - mudaStatusPausaLonga");
 		log.info("[idUsuario] {}", idUsuario);
+		String usuarioEmail = tokenService.getUsuarioByBearerToken(token)
+				.orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, token));
+		usuarioAppplicationService.mudaStatusPausaLonga(usuarioEmail, idUsuario);
 		log.info("[finaliza] UsuarioController - mudaStatusPausaLonga");
 	}
 }
